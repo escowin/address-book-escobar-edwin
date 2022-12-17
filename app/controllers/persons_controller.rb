32b66a18b18ab4eddea4798336dc8e4cdb6d29a9
escowin <edwin@escowinart.com>
2022-12-17 01:40:08 -0600

    bug | persons/new.html.erb:2 see note

--- app/controllers/persons_controller.rb
+++ app/controllers/persons_controller.rb
@@ -1,5 +1,6 @@
 # MVC | controller
 class PersonsController < ApplicationController
+  # CRUD | read
   def index # view | ./views/persons/index.html.erb
     @persons = Person.all # fetch person db request | displays the list of people
   end
@@ -7,4 +8,33 @@ class PersonsController < ApplicationController
   def show
     @person = Person.find(params[:id])
   end
+
+  # CRUD | create
+  # - instantiates new person
+  def new
+    @person = Person.new
+  end
+
+  # - instantiates with listed values & attempts to save. temp dummy values
+  def create
+    @person = Person.new(person_params)
+
+    if @person.save
+      redirect_to @person
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+    def person_params
+      params.require(:person).permit(
+        :firstname,
+        :middlename,
+        :lastname,
+        :ssn,
+        :dob,
+        :comment
+      )
+    end
 end
--- app/models/person.rb
+++ app/models/person.rb
@@ -1,2 +1,9 @@
 class Person < ApplicationRecord
+    # validates :salutation, presence: true
+    validates :firstname, presence: true
+    # validates :middlename, presence: true
+    validates :lastname, presence: true
+    # validates :ssn, presence: true
+    # validates :dob, presence: true
+    # validates :comment, presence: true
 end
