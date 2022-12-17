83c9abeb9e376d279e66fa8d70481112c70989ad
escowin <edwin@escowinart.com>
2022-12-17 00:46:22 -0600

    link_to method w/ concatenation

--- app/controllers/persons_controller.rb
+++ app/controllers/persons_controller.rb
@@ -3,4 +3,8 @@ class PersonsController < ApplicationController
   def index # view | ./views/persons/index.html.erb
     @persons = Person.all # fetch person db request | displays the list of people
   end
+
+  def show
+    @person = Person.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   root "persons#index"
 
   # CRUD operations
-  # - read | getting the individuals in contacts.
-  get "/persons", to: "persons#index" # controller | ./controllers/persons_controller.rb 
+  # - persons mvc, ./controllers/persons_controller.rb
+  resources :persons
 end
