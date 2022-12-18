9f45adafdd4fe633e810efb182b88af338bde579
escowin <edwin@escowinart.com>
2022-12-17 19:55:54 -0600

    Person model | 1:M; optional validations w/ length

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,9 +1,14 @@
 class Person < ApplicationRecord
+    # relationship with secondary models
+    has_many :addresses
+    has_many :emails
+    has_many :phone_numbers
+
     # validates :salutation, presence: true
     validates :firstname, presence: true
     # validates :middlename, presence: true
     validates :lastname, presence: true
-    # validates :ssn, presence: true
-    # validates :dob, presence: true
+    validates :ssn, length: { maximum: 9 }
+    validates :dob, length: { maximum: 8 }
     # validates :comment, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_16_215037) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_18_013805) do
+  create_table "addresses", force: :cascade do |t|
+    t.string "street"
+    t.string "town"
+    t.string "zipcode"
+    t.string "state"
+    t.string "country"
+    t.integer "person_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["person_id"], name: "index_addresses_on_person_id"
+  end
+
+  create_table "emails", force: :cascade do |t|
+    t.string "email"
+    t.text "comment"
+    t.integer "person_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["person_id"], name: "index_emails_on_person_id"
+  end
+
   create_table "people", force: :cascade do |t|
     t.string "salutation"
     t.string "firstname"
@@ -23,4 +44,16 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_16_215037) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "phone_numbers", force: :cascade do |t|
+    t.string "phone"
+    t.text "comment"
+    t.integer "person_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["person_id"], name: "index_phone_numbers_on_person_id"
+  end
+
+  add_foreign_key "addresses", "people"
+  add_foreign_key "emails", "people"
+  add_foreign_key "phone_numbers", "people"
 end
