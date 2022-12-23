c2c7c6d22dfb4fbecbcf8d05d60e51025b12c0e6
escowin <edwin@escowinart.com>
2022-12-23 05:06:43 -0600

    db | postgres

--- test/models/address_test.rb
+++ test/models/address_test.rb
@@ -1,6 +1,9 @@
 require "test_helper"
 
 class AddressTest < ActiveSupport::TestCase
+
+  test "adding an address" do
+    address = Address.submitted 
   # test "the truth" do
   #   assert true
   # end
