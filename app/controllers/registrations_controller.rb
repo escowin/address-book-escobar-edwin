c61010093f8213ce3539267b717a3f30d70a3671
escowin <edwin@escowinart.com>
2022-12-22 19:01:02 -0600

    responsive | home/index; routes | sessions & reg redirects to people path

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -7,7 +7,7 @@ class RegistrationsController < ApplicationController
         @user = User.new(user_params)
         if @user.save
             session[:user_id] = @user.id
-            redirect_to root_path, notice: "Sign up successful"
+            redirect_to people_path, notice: "Sign up successful"
         else
             render :new
         end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
         user = User.find_by(email: params[:email])
         if user.present? && user.authenticate(params[:password])
             session[:user_id] = user.id
-            redirect_to root_path, notice: "Log in successful"
+            redirect_to people_path, notice: "Log in successful"
         else
             flash[:alert] = "Invalid email or pasword"
             render :new
