fa95b8ec8a545b135a0b87b9cb3e438b5b7b55c7
escowin <edwin@escowinart.com>
2022-12-23 05:38:27 -0600

    tdd | address tests 2.0

--- test/models/address_test.rb
+++ test/models/address_test.rb
@@ -1,12 +1,21 @@
 require "test_helper"
 
 class AddressTest < ActiveSupport::TestCase
-
-  test "address count" do
-    assert_equal 2, Address.count
-  end
-
   # test "the truth" do
   #   assert true
   # end
+
+  # testing against fixtures
+  test "address_count" do
+    assert_equal 2, Address.count
+  end
+
+  test "find one" do
+    assert_equal "51st St", addresses(:one).street
+  end
+
+  test "find all by street" do
+    assert_equal 2, addresses.length
+  end
+  
 end
