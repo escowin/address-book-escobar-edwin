31929355b085ba28b791ed5585847d60b7746cc2
escowin <edwin@escowinart.com>
2022-12-23 06:29:12 -0600

    tdd | phone numbers (phone: integer)

--- test/models/phone_number_test.rb
+++ test/models/phone_number_test.rb
@@ -4,4 +4,21 @@ class PhoneNumberTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+    # testing against fixtures
+    test "phone number count" do
+      assert_equal 2, PhoneNumber.count
+    end
+  
+    test "find one" do
+      assert_equal 5121114000, phone_numbers(:one).phone
+    end
+  
+    test "find all" do
+      assert_equal 2, phone_numbers.length
+    end
+
+    test "null values" do
+      assert_equal nil, phone_numbers(:one).comment
+    end
 end
